eae544770ea8d7a9ad7bbf7929e573b99df6113f
Benjamin Roth <beroth@cs.umass.edu>
2015-10-09 12:25:38 -0400

    printing of tokens

--- src/main/scala/cc/factorie/epistemodb/types/DataFromWikipedia.scala
+++ src/main/scala/cc/factorie/epistemodb/types/DataFromWikipedia.scala
@@ -2,33 +2,57 @@ package cc.factorie.epistemodb.types
 
 import java.io.File
 
-import cc.factorie.app.nlp.Token
+import cc.factorie.app.nlp.{Sentence, Token}
 import edu.umass.cs.iesl.serialized_wikipedia.LoadWikipedia
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Created by beroth on 10/8/15.
  */
 object DataFromWikipedia {
+
+
+  def getNounsAndPatterns(sentence: Sentence): Seq[String] /* Seq[(String, Seq[String])]*/ = {
+    val tokens = sentence.tokens.zipWithIndex
+
+    val nouns = tokens.foldLeft((false, new ArrayBuffer[ArrayBuffer[String]]))((nounBuffer, tokenIndex) => {
+      val tok = tokenIndex._1
+
+      val lastTagWasNoun = nounBuffer._1
+      val buf: ArrayBuffer[ArrayBuffer[String]] =  nounBuffer._2
+
+      if (tok.posTag.categoryValue.startsWith("N")) {
+        val currNounBuf = if (lastTagWasNoun) {
+          buf.last
+        } else {
+          val nbuf = new ArrayBuffer[String]
+          buf+=nbuf
+          nbuf
+        }
+        currNounBuf+=tok.string
+        (true, buf)
+      } else {
+        (false, buf)
+      }
+    })._2
+
+    nouns.map(_.mkString(" ")).toSeq
+  }
+
   def main(args: Array[String]) : Unit = {
     LoadWikipedia.loadFile(new File(args(0)), "UTF-8").foreach{
       f =>
-        print("DOCUMENT: ")
-        println(f.name)
-
+        //print("DOCUMENT: ")
+        //println(f.name)
         for (s <- f.sentences) {
           val tokens = s.tokens
           val sString = tokens.map(tok => tok.string + "/" + tok.posTag.categoryValue.toString).mkString(" ")
           println(sString)
-
-          /*val posTags = s.posTags
-
-          if (tokens.length == posTags.length) {
-            val sString = tokens.zip(posTags).map(tokTag => tokTag._1.po + "/" + tokTag._2.toString).mkString(" ")
-            println(sString)
-          }*/
+          for (n <- getNounsAndPatterns(s)) {
+            println(s)
+          }
         }
-
-//println(f.owplString(Iterable((t:Token) => t.string, (t:Token) => t.posTag)))
     }
 
   }
